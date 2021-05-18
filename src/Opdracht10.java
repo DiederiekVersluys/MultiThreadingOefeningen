1226ff4ff9f40b85ef94890eec9b1ccf5abe51f3
DiederiekVersluys <80745300+DiederiekVersluys@users.noreply.github.com>
2021-05-18 09:28:03 +0200

    Update Opdracht10.java

--- src/Opdracht10.java
+++ src/Opdracht10.java
@@ -1,18 +1,17 @@
 import java.text.SimpleDateFormat;
-import java.time.LocalTime;
 import java.util.*;
 
 public class Opdracht10 {
     public static void main(String[] args) {
 
-        Date currentDate = new Date();
-        SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm:ss");
+
         Timer timer = new Timer();
         TimerTask task = new TimerTask() {
             @Override
             public void run() {
+                Date currentDate = new Date();
+                SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm:ss");
                 System.out.println(timeFormat.format(currentDate));
-                currentDate.setSeconds(currentDate.getSeconds()+1);
             }
         };
         timer.schedule(task, 0, 1000);
